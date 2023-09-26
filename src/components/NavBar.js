7ef01a43c6c72ce41c619ff574d25f779dc6af7c
Sunil H N <sunilh.n27@gmail.com>
2023-09-27 00:27:53 +0530

    Added mobile view for NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,51 @@
-import React from 'react'
-
+"use client"
+import React, { useState } from 'react';
+import Logo from '@/asserts/logo.png'
+import Image from 'next/image'
+import Link from 'next/link'
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { AiOutlineClose } from "react-icons/ai";
 function NavBar() {
-  return (
-    <div>NavBar</div>
-  )
+
+    const [showSubmenu, setShowSubmenu] = useState(false);
+    const [mobileView, setMobileView] = useState(true);
+    const toggleSubmenu = () => {
+        setShowSubmenu(!showSubmenu);
+    };
+
+    const handleView = () => {
+        setMobileView(!mobileView);
+    };
+
+    return (
+        <section className='w-auto h-[6rem] '>
+            <nav className='md:w-[73rem]  mx-auto flex justify-between items-center text-center  h-[6rem] px-3'>
+                <Image src={Logo} className='w-[8.8rem] md:w-[8.8rem] md:h-[2rem]' />
+                <div className='hidden md:flex gap-10 font-extrabold group-[5]:'>
+                    <Link href={"/"}>Home</Link>
+                    <Link href={"/"}>About</Link>
+                    <Link href={"/"}>Services</Link>
+                    <Link href={"/"}>Portfolio</Link>
+                    <Link href={"/"}>Blogs</Link>
+                    <Link href={"/"}>Contact</Link>
+                </div>
+                <div className='hidden md:flex'>
+                    <button className={`bg-[#F14836] text-white px-7 py-4 rounded-full `}>Download</button>
+
+                </div>
+                <div className='md:hidden'>
+                    {mobileView ? (
+                        <AiOutlineClose onClick={handleView} className='w-[1.5rem] h-[1.5rem]' />
+                    ) : (
+                        <GiHamburgerMenu onClick={handleView} className='w-[1.5rem] h-[1.5rem]' />
+                    )}
+                </div>
+            </nav>
+
+        </section >
+    )
 }
 
 export default NavBar
+
+
