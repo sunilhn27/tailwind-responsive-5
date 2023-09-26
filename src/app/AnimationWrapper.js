91ea9a81218f92793cbf26de89290564d91eca3c
Sunil H N <sunilh.n27@gmail.com>
2023-09-27 00:25:55 +0530

    added animation

---
+++ src/app/AnimationWrapper.js
@@ -0,0 +1,47 @@
+import { motion } from "framer-motion";
+
+function AnimationTop({ children, delay }) {
+  return (
+    <motion.div
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true }}
+      transition={{
+        duration: 1.2,
+        ease: [0.17, 0.67, 0.83, 0.91],
+        delay: delay,
+      }}
+      variants={{
+        visible: { y: 0, opacity: 1 },
+        hidden: { y: 50, opacity: 0 },
+      }}
+    >
+      {children}
+    </motion.div>
+  );
+}
+
+
+function AnimationLeft({ children, delay }) {
+    return (
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true }}
+        transition={{
+          duration: 1.2,
+          ease: [0.17, 0.67, 0.83, 0.91],
+          delay: delay,
+        }}
+        variants={{
+          visible: { x: 0, opacity: 1 },
+          hidden: { x: 50, opacity: 0 },
+        }}
+      >
+        {children}
+      </motion.div>
+    );
+  }
+
+  export { AnimationTop, AnimationLeft };
+  
